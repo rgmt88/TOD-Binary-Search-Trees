817db5bd5ae97d2a5fca572f449d629293ddeded
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2024-06-15 17:18:30 -0500

    Added prettyPrint function

--- index.js
+++ index.js
@@ -35,4 +35,17 @@ function createTree(array) {
     };
 }
 
-console.log(createTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.rightChild !== null) {
+      prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.leftChild !== null) {
+      prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+ 
+prettyPrint(createTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]).root);
